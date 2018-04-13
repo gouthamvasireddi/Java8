5b5d96903b8154fe97debf08cf2ade1b077ff40f
Goutham <goutham.vasireddi@gmail.com>
2018-04-13 01:58:30 -0500

    cleanup

--- src/main/java/Foreach.java
+++ src/main/java/Foreach.java
@@ -1,9 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Foreach {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		List<Integer> l = Arrays.asList(1,2,3,4,5);
+				
+				
+		Consumer<Integer> con = new Consumer<Integer>(){
+			public void accept(Integer i){
+				System.out.println(i);
+			}
+		};
+	 	
+		l.forEach(con);
 	}
 
 }
